73e292ea70b239d1c7e6e02b60987620af2e65c2
amanssa <99044445+amanssa@users.noreply.github.com>
2022-02-23 09:46:37 -0500

    Update and rename FInal_Project.py to Final_Project.py

---
+++ Final_Project.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+MA305 - Introduction to Scientific Computing
+Final Project
+
+Created on Fri Feb  4 13:23:26 2022
+
+@authors: Leila Alamos, Alexa Manssa, Harley Valdez, Ryan Hanrahan, Brett Bartoshevich
+
+Description: Image Processing and Recognition - We can recognize objects within a camera’s frame of reference and accurately 
+             identify the object to the user using imported data bases for reference. 
+"""
+
+#importing object databases
+
+#Currently files are too large to pull into GitHub
